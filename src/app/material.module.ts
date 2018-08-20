6d68aa668852e63df7e313216fb209b6828a2d82
Zakir Nuriev <zakir.nuriev@caspio.com>
2018-08-20 13:33:43 +0300

    Create app structure

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { MatSidenavModule } from '@angular/material';
+
+@NgModule({
+	imports: [MatSidenavModule],
+	exports: [MatSidenavModule]
+})
+export class MaterialModule {}
