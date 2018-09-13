7e8fa7fd5a60768dafb778b3c37007d00dbb3f6b
Zakir Nuriev <zakir.nuriev@caspio.com>
2018-09-13 11:44:15 +0300

    Update to functions configuration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,7 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest(
-	async (request, response) => {
-		console.log(request);
-		response.send('Hello from Firebase!');
-	}
-);
+export const helloWorld = functions.https.onRequest((request, response) => {
+	console.log(request);
+	response.send(process.versions);
+});
