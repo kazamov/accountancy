7e6fff22353417dcd88cf3ab0435c385ac350180
Zakir Nuriev <zakir.nuriev@caspio.com>
2018-09-13 13:41:58 +0300

    Update onCreateUser function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,34 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-	console.log(request);
-	response.send(process.versions);
-});
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+export const onUserCreate = functions
+	.region('europe-west1')
+	.auth.user()
+	.onCreate(async (user, context) => {
+		console.log(user.toJSON());
+		console.log(context);
+
+		try {
+			const userDocRef = db.collection('users').doc(user.uid);
+			await userDocRef.set({
+				email: user.email
+			});
+
+			['Продукты', 'Проезд', 'Здоровье', 'Коммунальные услуги'].forEach(
+				async categoryName => {
+					const categoryDoc = await userDocRef
+						.collection('categories')
+						.add({
+							id: '',
+							name: categoryName
+						});
+					await categoryDoc.update({ id: categoryDoc.id });
+				}
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	});
